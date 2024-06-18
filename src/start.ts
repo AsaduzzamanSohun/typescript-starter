0d29c779ac791741f71b23617d4073e9e7957eac
AsaduzzamanSohun <asaduzzamansohun5@gmail.com>
2024-06-18 22:44:53 +0600

    typescrit starter

---
+++ src/start.ts
@@ -0,0 +1,59 @@
+// let myName: string = 'Asaduzzaman Sohun';
+// myName = 'Somethings';
+// myName = myName.toUpperCase();
+// console.log(myName);
+
+// let amount: number = 20;
+// amount = 21
+// console.log(amount);
+
+// let isAwesome: boolean = true;
+// isAwesome = false;
+
+
+// let tax: number | string = 10;
+// tax = 100;
+// tax = '$10';
+
+// let requestStatus: 'pending' | 'success' | 'error' = 'pending';
+// requestStatus = 'success';
+// requestStatus = 'error';
+
+// let typeFree: any = 22;
+// typeFree = 'No type specific';
+// typeFree = true;
+
+
+// -------------------- Array ---------------------
+
+// const techs = ['HTML', 'CSS', 'Tailwind', 'JS', 'DOM', 'API', 'React', 'Firebase', 'MongoDB', 'Express', 'Tanstack Queries', 'JSON Web Token'];
+
+// let foundTech: string | undefined;
+
+// for (let tech of techs) {
+//     if (tech === 'Firebase') {
+//         foundTech = tech;
+
+//         foundTech = foundTech.toUpperCase();
+//         break;
+//     }
+
+// }
+
+// console.log(foundTech?.length);
+
+
+// Problem: 1
+let orderStatus: 'proccessing' | 'shipped' | 'delivered' = 'proccessing';
+orderStatus = "shipped";
+orderStatus = "delivered";
+
+// Problem: 2
+let discount: number | string = 20;
+discount = '20%';
+
+const prices: number[] = [20, 12, 16, 18];
+const fruits: string[] = ['apple', 'guava', 'berry'];
+const languages = ['bangla', 'english', 'french', 'german', 23, 221];
+
+
